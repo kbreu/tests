67d6071572f3170f68b595b51f2cef32af5f0261
kbreu <kbreu@vz.net>
2013-09-25 15:22:01 +0200

    splitter impl, added locations

---
+++ data.js
@@ -0,0 +1,199 @@
+var arr = [{
+    "name" : "Kita",
+    "address" : "Simplonstraße 16, 10245 Berlin",
+    "description" : "Öffnungszeiten<br/>Montag bis Freitag 6:00-18:00 Uhr<br/>Tel. 0302910614<br/>Email <a href='mailto:kita-simplonstrasse@bik-ev.de'>kita-simplonstrasse@bik-ev.de</a>",
+    "lat" : 52.50819661144704,
+    "lng" : 13.458948233868341,
+    "icon" : "img/map/kita.jpg"
+}, {
+    "name" : "Briefkasten",
+    "address" : "Wühlischstr. 24, 10245 Berlin",
+    "description" : "Leerungszeiten<br/>Mo - Fr 15:15    17:15    21:30</br>Sa  12:15<br/>So  09:00    21:30",
+    "lat" : 52.50912743213321,
+    "lng" : 13.459627375463242,
+    "icon" : "img/map/lettericon.gif"
+}, {
+    "name" : "Briefkasten",
+    "address" : "Sonntagstr. 1, 10245 Berlin",
+    "description" : "Leerungszeiten<br/>Mo - Fr 13:00    17:15    21:30<br/>Sa  12:15<br/>So  09:00    21:30",
+    "lat" : 52.504513271260144,
+    "lng" : 13.467909835581963,
+    "icon" : "img/map/lettericon.gif"
+}, {
+    "name" : "Packstation 114",
+    "address" : "Revaler Str. 2, 10243 Berlin",
+    "description" : "Kaiser's Markt<br/>Zugangszeiten<br/>täglich rund um die Uhr",
+    "lat" : 52.50878,
+    "lng" : 13.449589796295186,
+    "icon" : "img/map/packstation.jpg"
+}, {
+    "name" : "Postbank Finanzcenter",
+    "address" : "Frankfurter Allee 71-77, 10247 Berlin",
+    "description" : "Öffnungszeiten<br/>Mo - Fr 09:00 - 18:30<br/>Sa  09:30 - 13:00",
+    "lat" : 52.51453430000001,
+    "lng" : 13.468448399999943,
+    "icon" : "img/map/post.png"
+}, {
+    "name" : "Rewe",
+    "address" : "Revaler Straße 33, 10245 Berlin",
+    "description" : "030-29368761<br/>Geöffnet Mo-Sa 7-22 Uhr",
+    "lat" : 52.505776943592544,
+    "lng" : 13.459300498008702,
+    "icon" : "img/map/rewe.png"
+}, {
+    "name" : "Lidl",
+    "address" : "Boxhagener Str. 76-78, 10245 Berlin",
+    "description" : "Geöffnet Mo. - Sa. 8 - 22 Uhr",
+    "lat" : 52.50762563527553,
+    "lng" : 13.469050310554849,
+    "icon" : "img/map/lidl.bmp"
+}, {
+    "name" : "Aldi",
+    "address" : "Revaler Straße 33, 10245 Berlin‎",
+    "description" : "Geöffnet Mo-Sa 08:00-20:00 (???)",
+    "lat" : 52.5058422479773,
+    "lng" : 13.460196355819676,
+    "icon" : "img/map/aldi.bmp"
+}, {
+    "name" : "Briefkasten",
+    "address" : "Krossener Str. 1, 10245 Berlin",
+    "description" : "Leerungszeiten<br/>Mo - Fr 13:00    15:15    17:15    21:30<br/>Sa  12:15<br/>So  09:00    21:30",
+    "lat" : 52.509717540202594,
+    "lng" : 13.46397272948684,
+    "icon" : "img/map/lettericon.gif"
+}, {
+    "name" : "Zu Hause",
+    "address" : "Simplonstraße 46, 10245 Berlin",
+    "description" : "",
+    "lat" : 52.50592979796682,
+    "lng" : 13.463195780426076,
+    "icon" : "img/map/zuhause.jpg"
+}, {
+    "name" : "Deutsche Bank",
+    "address" : "Möllendorffstraße 117, 10367 Berlin",
+    "description" : "Öffnungszeiten<br/>Montag: 10:00 - 18:00<br/> Dienstag: 10:00 - 18:00 <br/>Mittwoch: 10:00 - 16:00 <br/>Donnerstag: 10:00 - 18:00 <br/>Freitag: 10:00 - 16:00 ",
+    "lat" : 52.51486,
+    "lng" : 13.477730000000065,
+    "icon" : "img/map/deutschebank.jpg"
+}, {
+    "name" : "Rewe",
+    "address" : "Am Ostbahnhof 9, 10243 Berlin",
+    "description" : "030-29365952<br/>Geöffnet Mo-So 7-24 Uhr",
+    "lat" : 52.50961055435834,
+    "lng" : 13.434994472213702,
+    "icon" : "img/map/rewe.png"
+}, {
+    "name" : "Commerzbank",
+    "address" : "Frankfurter Allee 24 - 26, 10247 Berlin",
+    "description" : "Filialöffnungszeiten<br/>Montag   09:00-13:00   14:00-16:00<br/>Dienstag    09:00-13:00   14:30-18:00<br/>Mittwoch    09:00-13:00   14:00-16:00<br/>Donnerstag  09:00-13:00   14:30-18:00<br/>Freitag 09:00-14:00   ",
+    "lat" : 52.5150065,
+    "lng" : 13.459470799999963,
+    "icon" : "img/map/commerzbank.jpg"
+}, {
+    "name" : "Sparkasse",
+    "address" : "Boxhagener Str. 55, 10245 Berlin",
+    "description" : "<strong>Öffnungszeiten</strong><br/>Mo, Mi, Fr 9.30-15.00<br/>Di, Do 9.30-18.00",
+    "lat" : 52.507279603040644,
+    "lng" : 13.467849795141206,
+    "icon" : "img/map/sparkasse.ico"
+}, {
+    "name" : "Immobilien Scout GmbH",
+    "address" : "Andreasstr. 10, 10243 Berlin",
+    "description" : "",
+    "lat" : 52.512594,
+    "lng" : 13.431107999999995,
+    "icon" : "img/map/immobilienscout.jpg"
+}, {
+    "name" : "Geldautomat",
+    "address" : "BOXHAGENER STR. 55, BERLIN, 10245",
+    "description" : "",
+    "icon" : "img/map/visa.jpg",
+    "lat" : 52.50729511231058,
+    "lng" : 13.467869911708817
+}, {
+    "name" : "Geldautomat",
+    "address" : "SIMPLONSTR. 10/12, BERLIN, 10245",
+    "description" : "",
+    "icon" : "img/map/visa.jpg",
+    "lat" : 52.5090823,
+    "lng" : 13.45748679999997
+}, {
+    "name" : "Geldautomat",
+    "address" : "REVALER STR. 2, BERLIN, 10243",
+    "description" : "",
+    "icon" : "img/map/visa.jpg",
+    "lat" : 52.50861021974609,
+    "lng" : 13.449697084655782
+}, {
+    "name" : "Geldautomat",
+    "address" : "Frankfurter Allee 70, Berlin, 10247",
+    "description" : "",
+    "icon" : "img/map/visa.jpg",
+    "lat" : 52.5142862,
+    "lng" : 13.467135299999995
+}, {
+    "name" : "Bezirksamt Friedrichshain - Kreuzberg",
+    "address" : "Frankfurter Allee 35 / 37, 10247 Berlin",
+    "description" : "Öffnungszeiten<br/>Montag 08:00 - 15.00 Uhr<br/>Dienstag 09:00 - 18:00 Uhr<br/>Mittwoch 08:00 - 13:00 Uhr (nur mit Termin)<br/>Donnerstag 09:00 - 18:00 Uhr (nur mit Termin)<br/>Freitag 08:00 - 13:00 Uhr<br/>Samstag Jeden 2. und 4. Samstag im Monat: 09:00 - 13:00 Uhr (nur mit Termin)",
+    "icon" : "img/map/berlin.ico",
+    "lat" : 52.51529,
+    "lng" : 13.461620000000039
+}, {
+    "name" : "Nil - Sudanesischer Imbiss",
+    "address" : "Grünberger Str. 52, 10245 Berlin",
+    "description" : "Telefon : 030 - 29 04 77 13<br/>Email : info@nil-imbiss.de<br/>Öffnungszeiten: täglich 11 Uhr mittag bis 24 Uhr",
+    "icon" : "img/map/nil.gif",
+    "lat" : 52.5118678,
+    "lng" : 13.455497599999944
+}, {
+    "name" : "Kaufland",
+    "address" : "Hermann-Blankenstein-Straße 38, Berlin",
+    "description" : "Mo. - Fr. 07:00 - 24:00 Uhr<br/>Sa. 07:00 - 23:30 Uhr",
+    "icon" : "img/map/kaufland.jpg",
+    "lat" : 52.52362353328227,
+    "lng" : 13.461926395770206
+}, {
+    "name" : "Rossmann",
+    "address" : "Warschauer Straße 73, 10243 Berlin",
+    "description" : "Öffnungszeiten<br/>Mo–Sa   09:00–21:00",
+    "icon" : "img/map/rossmann.png",
+    "lat" : 52.51219469999999,
+    "lng" : 13.452899000000002
+}, {
+    "name" : "Rossmann",
+    "address" : "Am Ostbahnhof 9, 10243 Berlin",
+    "description" : "Öffnungszeiten<br/>Mo–So   08:00–22:00",
+    "icon" : "img/map/rossmann.png",
+    "lat" : 52.50965626345084,
+    "lng" : 13.434769166656451
+}, {
+    "name" : "Rossmann",
+    "address" : "Frankfurter Allee 79, 10247 Berlin",
+    "description" : "Öffnungszeiten<br/>Mo–Fr   09:00–20:00, Sa  09:00–16:00",
+    "icon" : "img/map/rossmann.png",
+    "lat" : 52.51455,
+    "lng" : 13.469060000000013
+}, {
+    "name" : "dm-drogerie markt",
+    "address" : "Revaler Straße 2, 10243 Berlin",
+    "description" : "Öffnungszeiten<br/>MO-SA  09:00-22:00 Uhr",
+    "icon" : "img/map/dm.jpg",
+    "lat" : 52.508766940003746,
+    "lng" : 13.450169153442403
+}, {
+    "name" : "dm-drogerie markt",
+    "address" : "Frankfurter Allee 111, 10247 Berlin",
+    "description" : "Öffnungszeiten<br/>MO-SA  09:30-21:00 Uhr",
+    "icon" : "img/map/dm.jpg",
+    "lat" : 52.5141263,
+    "lng" : 13.474508900000046
+}, {
+    "name" : "Apotheke am Ostkreuz",
+    "address" : "Sonntagstraße 12, 10245 Berlin",
+    "description" : "030 2916212",
+    "icon" : "img/map/apotheke.png"
+}
+
+
+
+];
---
+++ script.js
@@ -0,0 +1,90 @@
+var map, mgr;
+var markers = [];
+var geocoder;
+
+google.maps.visualRefresh = true;
+
+function saveMarkerPositions() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].data.lat = markers[i].marker.getPosition().lat();
+        markers[i].data.lng = markers[i].marker.getPosition().lng();
+    }
+
+    console.log(JSON.stringify(arr));
+}
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+
+    var mapOptions = {
+        labels : false,
+        zoom : 16,
+        center : new google.maps.LatLng(52.506022, 13.463058),
+        mapTypeId : google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    // Kirchen
+    // Cafes
+    // Restaurants
+    // Icons
+    // Detailzone bei Klick
+    // Tankstellen
+    // Geldautomaten
+    // Kioske
+
+    for (var i = 0; i < arr.length; i++) {
+        console.log('i ', i);
+        console.log('arr[i] ', arr[i]);
+        (function(entry) {
+            console.log('entry ', entry);
+            if (entry.lat && entry.lng) {
+                var marker = new google.maps.Marker({
+                    position : new google.maps.LatLng(entry.lat, entry.lng),
+                    map : map,
+                    title : entry.name,
+                    icon : entry.icon,
+                    draggable : true,
+                    flat : true
+                });
+                markers.push({
+                    marker : marker,
+                    data : entry
+                });
+                google.maps.event.addListener(marker, 'click', function() {
+                    $('#details').html(entry.name + "<br/>" + entry.address + "<br/>" + entry.description);
+                });
+
+            } else if (entry.address) {
+                geocoder.geocode({
+                    'address' : entry.address
+                }, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        var marker = new google.maps.Marker({
+                            position : results[0].geometry.location,
+                            icon : entry.icon,
+                            map : map,
+                            title : entry.name,
+                            draggable : true,
+                            flat : true
+                        });
+                        entry.lat = results[0].geometry.location.lat();
+                        entry.lng = results[0].geometry.location.lng();
+                        markers.push({
+                            marker : marker,
+                            data : entry
+                        });
+                        google.maps.event.addListener(marker, 'click', function() {
+                            $('#details').html(entry.name + "<br/>" + entry.address + "<br/>" + entry.description);
+                        });
+                    } else {
+                        alert("Geocode was not successful for the following reason: " + status);
+                    }
+                });
+            }
+
+        })(arr[i]);
+    }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
